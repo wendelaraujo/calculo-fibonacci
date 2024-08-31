4ed8d577dca51f8b064ed89ad628c51e69337cd0
Wendel Araujo <141687961+wendelaraujo@users.noreply.github.com>
2024-08-31 17:12:05 -0300

    Add files via upload

---
+++ calculo-fibonacci.js
@@ -0,0 +1,13 @@
+// Inicializando as variáveis
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+// Estrutura de repetição semelhante ao "Enquanto"
+while (K < INDICE) {
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+// Exibindo o valor final de SOMA
+console.log("O valor final de SOMA é:", SOMA);
